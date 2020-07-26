8c096bd458e466e2e9ec0663054dff9a7d0a5925
fanghg2018 <fanghg2018@gmail.com>
2020-07-26 08:42:40 +0800

    7.28

--- public/GoogleSearch.js
+++ public/GoogleSearch.js
@@ -1,12 +1,12 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 const googleSearch = async (context) => {
-  const browser = await puppeteer.launch({ headless:false});
-  const page = await browser.newPage();
-  await page.goto('https://www.baidu.com');
-  await page.focus('#kw'); //关键字输入框的id. 注释2
-  await page.type('#kw',context); 
-  await page.click('#su'); //'百度一下'按钮的id. 注释3
-  await page.waitFor(3000);
-  await browser.close();
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.goto('https://www.baidu.com')
+  await page.focus('#kw') // 关键字输入框的id. 注释2
+  await page.type('#kw', context)
+  await page.click('#su') // '百度一下'按钮的id. 注释3
+  await page.waitFor(3000)
+  await browser.close()
 }
